491e5c8eaba9e585270ce61384b041b627fd48c2
Alastair Rankine <alastair@girtby.net>
2023-02-24 10:26:26 -0500

    better ui (stolen from centaur)

--- early-init.el
+++ early-init.el
@@ -1,3 +1,9 @@
 ;; -*- lexical-binding: t -*-
 
 (setq package-enable-at-startup nil)
+
+
+;; Faster to disable these here (before they've been initialized)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
