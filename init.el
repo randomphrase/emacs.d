6652a944de9f18fb9e0c307f04fdde575fd0f91e
Alastair Rankine <alastair@girtby.net>
2023-02-24 14:12:10 -0500

    enable consult-ripgrep

--- init.el
+++ init.el
@@ -254,7 +254,7 @@
          ;; ("M-s D" . consult-locate)
          ;; ("M-s g" . consult-grep)
          ;; ("M-s G" . consult-git-grep)
-         ;; ("M-s r" . consult-ripgrep)
+         ("M-s r" . consult-ripgrep)
          ;; ("M-s l" . consult-line)
          ;; ("M-s L" . consult-line-multi)
          ;; ("M-s k" . consult-keep-lines)
