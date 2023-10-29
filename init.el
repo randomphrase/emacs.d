5b09213ea9276d47ee5fd886d26dfd4f21b4a71b
Alastair Rankine <alastair@girtby.net>
2023-10-29 12:59:15 -0400

    init.el: put custom stuff into custom.el

--- init.el
+++ init.el
@@ -23,6 +23,13 @@
 
 (straight-use-package 'use-package)
 
+
+;; custom stuff goes into custom.el
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file 'noerror))
+
+
 ;; -- user interface
 
 ;; nice scrolling
@@ -500,17 +507,3 @@
 
 (server-mode 1)
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("1cae4424345f7fe5225724301ef1a793e610ae5a4e23c023076dc334a9eb940a" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
