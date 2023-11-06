bc961913e820eb0812d03acf4b47056dbe2bf422
Alastair Rankine <alastair@girtby.net>
2023-11-06 09:58:47 -0500

    use ef-themes

--- init.el
+++ init.el
@@ -53,24 +53,8 @@
   :hook (after-init . doom-modeline-mode)
   )
 
-(use-package doom-themes
-  :ensure t
-  :config
-  ;; Global settings (defaults)
-  ;; (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-  ;;       doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; ;; Enable custom neotree theme (all-the-icons must be installed!)
-  ;; (doom-themes-neotree-config)
-  ;; ;; or for treemacs users
-  ;; (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  ;; (doom-themes-treemacs-config)
-  ;; ;; Corrects (and improves) org-mode's native fontification.
-  ;; (doom-themes-org-config)
-  )
+(use-package ef-themes
+  :init (load-theme 'ef-night :no-confirm))
 
 (setq frame-title-format '("Emacs - " user-login-name "@" system-name ":%f"))
 
