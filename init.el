0f1b6daa13a8c1acafbfa402dedf7e8a572af1dd
Alastair Rankine <alastair@girtby.net>
2023-11-25 17:39:38 -0500

    init.el: added pyrightconfig-write

--- init.el
+++ init.el
@@ -492,6 +492,24 @@
   (add-hook 'python-ts-mode-hook 'eglot-ensure)
   )
 
+;; stolen from https://github.com/renzmann/.emacs.d
+(defun pyrightconfig-write (virtualenv)
+  "Write a `pyrightconfig.json' file at the Git root of a project
+with `venvPath' and `venv' set to the absolute path of
+`virtualenv'.  When run interactively, prompts for a directory to
+select."
+  (interactive "DEnv: ")
+  ;; Naming convention for venvPath matches the field for pyrightconfig.json
+  (let* ((venv-dir (tramp-file-local-name (file-truename virtualenv)))
+         (venv-file-name (directory-file-name venv-dir))
+         (venvPath (file-name-directory venv-file-name))
+         (venv (file-name-base venv-file-name))
+         (base-dir (vc-git-root default-directory))
+         (out-file (expand-file-name "pyrightconfig.json" base-dir))
+         (out-contents (json-encode (list :venvPath venvPath :venv venv))))
+    (with-temp-file out-file (insert out-contents))
+    (message (concat "Configured `" out-file "` to use environment `" venv-dir))))
+
 ;; -- typescript
 
 (use-package typescript-mode
