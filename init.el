8c3f25231df1fab544573a834cd8b47a05bb081a
Alastair Rankine <alastair@girtby.net>
2024-05-10 10:19:42 -0400

    use winum mode instead of ace-window

--- init.el
+++ init.el
@@ -84,7 +84,7 @@
 
 (use-package popper
   :ensure t ; or :straight t
-  :bind (("C-`"   . popper-toggle-latest)
+  :bind (("C-`"   . popper-toggle)
          ("M-`"   . popper-cycle)
          ("C-M-`" . popper-toggle-type))
   :init
@@ -108,8 +108,25 @@
 				 )))
   )
 
-(use-package ace-window
-  :bind ("M-o" . ace-window)
+;; (use-package ace-window
+;;   :bind ("M-o" . ace-window)
+;;   )
+
+(use-package winum
+  :bind
+  (:map winum-keymap
+	("M-0" . winum-select-window-0-or-10)
+	("M-1" . winum-select-window-1)
+	("M-2" . winum-select-window-2)
+	("M-3" . winum-select-window-3)
+	("M-4" . winum-select-window-4)
+	("M-5" . winum-select-window-5)
+	("M-6" . winum-select-window-6)
+	("M-7" . winum-select-window-7)
+	("M-8" . winum-select-window-8)
+	("M-9" . winum-select-window-9))
+  :init
+  (winum-mode)
   )
 
 ;; -- global keys
