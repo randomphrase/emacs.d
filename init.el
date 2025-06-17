ee01320389e6627ff8c4254f4753e73176c28f45
Alastair Rankine <alastair@girby.net>
2025-06-17 11:13:08 -0400

    init.el: copilot config

--- init.el
+++ init.el
@@ -594,6 +594,13 @@
 (use-package copilot
   :straight (:host github :repo "copilot-emacs/copilot.el" :files ("*.el"))
   :ensure t
+  :custom
+  (copilot-max-char 200000)
+  :config
+  (add-to-list 'copilot-indentation-alist
+	       '(emacs-lisp-mode . 2)
+	       '(makefile-mode . 4)
+	       )
   :hook (prog-mode . copilot-mode)
   :bind (:map copilot-mode-map
               ("M-C-<return>" . copilot-accept-completion)
