30096c2a525b1c86727c6ec5c107f1515b183971
Alastair Rankine <alastair@girtby.net>
2025-11-17 20:52:10 -0500

    early-init: revert bad changes

--- early-init.el
+++ early-init.el
@@ -1,7 +1,6 @@
 ;;; early-init.el --- Preload tweaks -*- lexical-binding: t; -*-
 
-(setq package-enable-at-startup nil
-      load-prefer-newer t)
+(setq package-enable-at-startup nil)
 
 ;; Startup speed: relax GC & handlers during init
 (setq gc-cons-threshold most-positive-fixnum
@@ -9,9 +8,9 @@
 (defvar ar/default-file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 
-;; Reduce UI chrome during startup
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+;; Faster to disable these here (before they've been initialized)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
 
 ;;; early-init.el ends here
