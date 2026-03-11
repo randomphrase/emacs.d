dcba5197701d106023549ca0dd60291f6753807a
Alastair Rankine <alastair@girtby.net>
2026-03-10 20:59:13 -0400

    use pinentry to accept gpg passphrases

--- lisp/mod-git.el
+++ lisp/mod-git.el
@@ -6,6 +6,11 @@
   :if (executable-find "delta")
   :hook (magit-mode . magit-delta-mode))
 
+(use-package pinentry
+  :ensure t
+  :config
+  (setq epa-pinentry-mode 'loopback)
+  (pinentry-start))
 
 (provide 'mod-git)
 ;;; mod-git.el ends here
